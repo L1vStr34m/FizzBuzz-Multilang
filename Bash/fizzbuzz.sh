53660f0012cd41ea79228510f9a087bb4a71a392
L1vStr34m <226928132+L1vStr34m@users.noreply.github.com>
2025-09-04 21:56:43 +0100

    Add Bash one-liner FizzBuzz script

---
+++ Bash/fizzbuzz.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+# - Suguro | github.com/L1vStr34m
+for n in {1..100}; do ((( n % 15 == 0 )) && echo 'FizzBuzz') || ((( n % 5 == 0 )) && echo 'Buzz') || ((( n % 3 == 0 )) && echo 'Fizz') || echo $n; done
